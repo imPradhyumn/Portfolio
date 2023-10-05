41d2f3b919ea50600d0791de1a1f71b73dfed805
Sharma <pradhyumn.sharma@capgemini.com>
2023-10-05 23:08:33 +0530

    Added meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,29 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Pradhyumn's Portfolio",
-  description: "",
+  description: "A portfolio website for software developer",
+  themeColor: "dark",
+  authors: {
+    name: "Pradhyumn Sharma",
+    url: "https://www.linkedin.com/in/im-pradhyumn",
+  },
+  category: "website",
+  creator: "Pradhyumn Sharma",
+  keywords: [
+    "portfolio",
+    "web developer",
+    "front end",
+    "full stack",
+    "web design",
+    "react",
+    "python",
+    "java",
+    "css",
+    "html",
+    "nextjs",
+    "flask",
+    "javascript",
+  ],
 };
 
 export default function RootLayout({
@@ -21,6 +43,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="description" content="Personal Portfolio Website" />
+        <meta name="title" content="imPradhyumn" />
+        <meta name="type" content="website" />
+        <meta name="url" content="https://im-pradhyumn.vercel.sh" />
+      </Head>
       <body className={inter.className}>
         <Navbar />
         <main style={{ marginTop: "3rem" }}>{children}</main>
